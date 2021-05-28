d6ad6b8bce5c1e692bb1741937c4846816fd3c7f
Alec Flowers <alec.flowers00@gmail.com>
2021-05-28 14:40:06 +0200

    updated test

--- test.py
+++ test.py
@@ -14,16 +14,15 @@ def main():
     # Build a Network with two input units, two output units, 3 hidden layers of 25 units
     n_input = 2
     n_output =2
-    mod_list = [Linear(n_input, 10, 0.0, 1.0), Relu(),
-                Linear(10, 25, 0.0, 1.0), Relu(),
+    mod_list = [Linear(n_input, 25, 0.0, 1.0), Relu(),
                 Linear(25, 25, 0.0, 1.0), Relu(),
-                Linear(25, 10, 0.0, 1.0), Relu(),
-                Linear(10, n_output, 0.0, 1.0)]
+                Linear(25, 25, 0.0, 1.0), Relu(),
+                Linear(25, n_output, 0.0, 1.0)]
     # Train with MSE Loss
     criterion = MSEloss()
 
     # # Log the Loss
-    net = train_network(mod_list, criterion, train_data, train_labels, lr=1e-5, network_name='mse',
+    net = train_network(mod_list, criterion, train_data, train_labels, lr=1e-4, network_name='vanilla_network',
                         epochs=200, batch_size=5)
 
     # Compute and print final test error
@@ -31,14 +30,16 @@ def main():
 
     # Example using CrossEntropyLoss
 
-    # mod_list2 = [Linear(n_input, 10, 0.0, 1.0), Tanh(),
-    #              Linear(10, 50, 0.0, 1.0), Tanh(),
-    #              Linear(50, 50, 0.0, 1.0), Tanh(),
-    #              Linear(50, 10, 0.0, 1.0), Relu(),
-    #              Linear(10, n_output, 0.0, 1.0), LeakyRelu()]
+    # mod_list2 = [Linear(n_input, 10, 0.0, 1.0), LeakyRelu(),
+    #              Linear(10, 50, 0.0, 1.0), LeakyRelu(),
+    #              Linear(50, 100, 0.0, 1.0), LeakyRelu(),
+    #              Linear(100, 100, 0.0, 1.0), LeakyRelu(),
+    #              Linear(100, 50, 0.0, 1.0), LeakyRelu(),
+    #              Linear(50, 10, 0.0, 1.0), LeakyRelu(),
+    #              Linear(10, n_output, 0.0, 1.0)]
     # criterion2 = CrossEntropyLoss()
     #
-    # net2 = train_network(mod_list2, criterion2, train_data, train_labels, lr=1e-4, network_name='cross_entropy', batch_size=5)
+    # net2 = train_network(mod_list2, criterion2, train_data, train_labels, lr=1e-4, network_name='cross_entropy', batch_size=5, epochs=200)
     # test_network(net2)
 
 
